5e3943f671d4ebb13daee95e3f7d00568a109ba5
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-06-17 14:50:55 -0300

    Implement CourseGoalItem component

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.tsx
@@ -0,0 +1,19 @@
+import "./CourseGoalItem.css";
+
+const CourseGoalItem = (props: {
+  children: never;
+  id: string;
+  onDelete: (id: string) => void;
+}) => {
+  const deleteHandler = () => {
+    props.onDelete(props.id);
+  };
+
+  return (
+    <li className="goal-item" onClick={deleteHandler}>
+      {props.children}
+    </li>
+  );
+};
+
+export default CourseGoalItem;
